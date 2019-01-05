20a38a485286defe110ab51f41d40656f1f1ad56
vherasme <victor.herasme@sogeti.com>
2019-01-06 00:27:46 +0100

    Changes in the checkheap function

--- src/com/victor/trees/heaps/HeapExercises.groovy
+++ src/com/victor/trees/heaps/HeapExercises.groovy
@@ -2,27 +2,36 @@ package com.victor.trees.heaps
 
 class HeapExercises<T> {
 
-	static final Map<String, Closure> functionsMap = new HashMap<>()
+	static final Map<String, Closure> checkHeapMap = new HashMap<>()
 	static {
 
-		functionsMap.put("min", {Comparable[] heap, i ->
-			println "Min heap checking $i"
-			return !(heap[i] > heap[2*i - 1] || heap[i] > heap[2*i + 2])
+		checkHeapMap.put("min", {Comparable[] heap, lastNode ->
+			for (int i = 0; i < lastNode;i++) {
+				if ((heap[i] > heap[2*i - 1] || heap[i] > heap[2*i + 2]))
+					return false
+			}
+			return true
 		})
 
-		functionsMap.put("max", {Comparable[] heap, i ->
-			println "Max heap checking $i"
-			return !(heap[i] < heap[2*i - 1] || heap[i] < heap[2*i + 2])
+		checkHeapMap.put("max", {Comparable[] heap, lastNode ->
+
+			for (int i = 0; i < lastNode;i++) {
+				if ((heap[i] < heap[2*i - 1] || heap[i] < heap[2*i + 2]))
+					return false
+			}
+			return true
 		})
 	}
 
+	/*
+	 * Function to check if heap is either a valid max or min heap
+	 * @param heap This is the heap to examine
+	 * @param type This value should be either "min" or max
+	 * @return boolean 
+	 * */
 	static boolean checkHeap(Comparable[] heap, String type) {
 		int lastNode = (heap.length - 2).intdiv(2)
-		def isValidHeap = true
-		for (int i = 0; i < lastNode;i++) {
-			isValidHeap = functionsMap[type].call(heap, i)
-		}
-		return isValidHeap
+		return checkHeapMap[type].call(heap, lastNode)
 	}
 }
 
